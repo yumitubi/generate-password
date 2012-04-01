6dbce378b6018b39cb8df456e34e3ad85d047f88
le087 <mak.tomilov@gmail.com>
2012-04-01 21:06:59 +0600

    добавлена функция, генерирующая основной словарь правил

--- gen_pass/gen_dict.py
+++ gen_pass/gen_dict.py
@@ -25,7 +25,9 @@ def gen_dict_fsmb():
         file_r = openfile.readlines()
     except:
         print 'Файла не существует'
-
+    #------------------------------------------------------------
+    # генерим словарь вида {'gu': 7, 'gr': 20, 'ge': 7, 'ga': 8}
+    #------------------------------------------------------------        
     for st in file_r:
         for smb in st:
             if smb == ' ':
@@ -37,11 +39,45 @@ def gen_dict_fsmb():
                         dict_fsmb[key_dict] += 1
                     else:
                         dict_fsmb[key_dict] = 1
+    openfile.close()
     return dict_fsmb
 
+def gen_dict_othsmb():
+    """generate dictionary other symbols
+    """
+    dict_othsmb = {} #словарь правил для остальных символов
+    key_dict = ''
+    re_patt = re.compile('[A-Za-z]')
+    try:
+        openfile = open('/home/mak/devel/scripts/old_work/gen_pass/pereraboka.txt', 'r')
+        file_r = openfile.readlines()
+    except:
+        print 'Файла не существует'
+    #------------------------------------------------------------
+    # генерим словарь вида { 'ab':{'c':1, 'm':2}, 'bc':{'d':1} }
+    #------------------------------------------------------------
+    for st in file_r:
+        for smb in st:
+            if smb == ' ':
+                key_dict = ''
+            if re_patt.findall(smb):
+                key_dict = key_dict + smb.lower()
+                if len(key_dict) > 2:
+                    d_smb = key_dict[-3:-1]
+                    if dict_othsmb.has_key(d_smb):
+                        if dict_othsmb[d_smb].has_key(smb):
+                            dict_othsmb[d_smb][smb] += 1
+                        else:
+                            dict_othsmb[d_smb][smb] = 1
+                    else:
+                        dict_othsmb[d_smb] = { smb:1 }
+    print dict_othsmb
+                        
+#    openfile.close()
+#    return dict_othsmb
+gen_dict_othsmb()
 
-
-        
+    
 def get_passwd():
     """generate paswd
     """
